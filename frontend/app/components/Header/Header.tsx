290e69d440337be831cc39777309a6b6855472db
sdnn44 <slidennnq@gmail.com>
2024-07-02 23:25:53 +0200

    Improved responsiveness of Header

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -19,12 +19,12 @@ export default function Header({ }: Props) {
 
   return (
     <div className='flex flex-row bg-[#020817] border-b-2 h-16 w-full justify-between'>
-      <div className="flex flex-row items-center w-full justify-between md:justify-start">
+      <div className="flex flex-col md:flex-row md:items-center w-1/2 p-1 gap-1 justify-between md:justify-start">
         <p className='ml-5 md:mx-3 text-[12px] md:text-xl font-bold text-white tracking-widest flex flex-row gap-1'>
           <div onClick={() => homePage()} className='cursor-pointer'><span className="text-[#8884d8]">ADMIN</span> DASHBOARD</div>
         </p>
         {token ? (
-          <Button className="rounded-xl bg-[#8884d8] hover:bg-[#a6a1f7] text-white hover:text-white/90 duration-300 ease-in-out">
+          <Button className="text-[12px] md:text-base rounded-xl bg-[#8884d8] hover:bg-[#a6a1f7] text-white hover:text-white/90 duration-300 ease-in-out">
             <Link href="/admin/dashboard">Panel admina</Link>
           </Button>
         ) : (
